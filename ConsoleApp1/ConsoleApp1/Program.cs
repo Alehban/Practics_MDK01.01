b20153ee53a376fef02dfa0eb9927b06b1a91da4
Alehban <fabulousbeast00@gmail.com>
2025-02-07 14:36:21 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите y");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double result = Math.Pow((x + 1 / x - 1), x) + 18 * x * Math.Pow(y, 2);
+            Console.WriteLine("Для x = {0:f4} и y = {1:f4} ", x, y);
+            Console.WriteLine("Результат = {0:f4}", result);
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите a");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите c");
+            double c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите d");
+            double d = Convert.ToDouble(Console.ReadLine());
+            double result = (a / b) / (c / d);
+            Console.WriteLine("Для дробей {0}/{1} и {2}/{3} ", a, b, c, d);
+            Console.WriteLine("Результат = {0:f4}", result);
+        }
+    }
+}
