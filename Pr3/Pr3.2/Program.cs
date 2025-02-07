1d57edef9c68903093bba012c30bd0ca190d80c6
Alehban <fabulousbeast00@gmail.com>
2025-02-07 19:59:20 +0300

    Практическая 3

---
+++ Pr3/Pr3.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double distance = 10;
+        double totalDistance = 0;
+
+        for (int i = 1; i <= 7; i++)
+        {
+            totalDistance += distance;
+            distance *= 1.1;
+        }
+
+        Console.WriteLine($"Суммарное расстояние за 7 дней: {totalDistance} км");
+    }
+}
---
+++ Pr3/Pr3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Pr3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите n");
+            uint n;
+            int i;
+            n = Convert.ToUInt32(Console.ReadLine());
+            double S = 0;
+            for (i = 1; i <= n; i++) { S += Math.Pow(n, 7); i++; }
+            Console.WriteLine("Результат ={0}", S);
+        }
+    }
+}
+
